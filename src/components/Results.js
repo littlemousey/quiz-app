297691bf865c8e7b46535ee01d85480c380256b6
Ans de Nijs <ans.de.nijs@frontmen.nl>
2019-01-10 15:39:31 +0100

    Results page includes overview questions

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,10 +2,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Results = ({ questionsRight }) => (
+function renderOverview(questions) {
+  return questions.map((item, index) => (
+    <div key={`${item.userAnswer}${index}`}>
+      <h4>Question Asked</h4>
+      {item.question}
+      <h4>Your Answer:</h4>
+      {item.userAnswer}
+      <h4> Correct Answer</h4>
+      {item.correctAnswer}
+    </div>
+  ));
+}
+
+const Results = ({ questionsRight, recordedQuestions }) => (
   <div>
     <h3>That was it!</h3>
-    <p>Amount of questions you answered right: {questionsRight}</p>
+    {recordedQuestions && (
+      <div>
+        <p>Amount of questions you answered right: {questionsRight}</p>
+        <h2>Overview</h2>
+        <div>{renderOverview(recordedQuestions)}</div>
+      </div>
+    )}
   </div>
 );
 
@@ -13,4 +32,5 @@ export default Results;
 
 Results.propTypes = {
   questionsRight: PropTypes.number,
+  recordedQuestions: PropTypes.array,
 };
